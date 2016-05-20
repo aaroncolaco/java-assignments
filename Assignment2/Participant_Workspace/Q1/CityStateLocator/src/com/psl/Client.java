9a59c9fd8afdd89ae84115c99a0c39b7aa4c0243
aarongoa <taipan93@gmail.com>
2016-05-20 20:07:40 +0530

    Assignment 2 Q1 part 2 & 3 done Code optimizations

--- Assignment2/Participant_Workspace/Q1/CityStateLocator/src/com/psl/Client.java
+++ Assignment2/Participant_Workspace/Q1/CityStateLocator/src/com/psl/Client.java
@@ -11,10 +11,13 @@ public class Client {
 	public static void main(String[] args) throws Exception{
 		//Call your methods from here  to test the code implemented
             
-            Map<String, List<String>> dataMap = new HashMap<String, List<String>>();
+            Map<String, List<String>> dataMap = new HashMap<>();    // okay even if not initialized because assigned later to the returned value from the function call. But have to allocate mem like this if we will store value ourselves.
+            List <String> citiesList;
+            String stateString;
             
             DataManagerImpl dataManagerImpl = new DataManagerImpl();
             
+            // TEST -populate the map
             dataMap = dataManagerImpl.populateCityDataMap("StateCityDetails.txt");
             
             //To view dataMap value sets
@@ -22,5 +25,17 @@ public class Client {
             for (String string : dataMap.keySet()) {
                 System.out.println( string + "--> " + dataMap.get(string));
             }
+            
+            
+            // TEST - get cities from a state
+            citiesList = dataManagerImpl.getCities(dataMap, "Goa");
+            
+            System.out.println(citiesList);
+            
+            
+            // TEST - get state a city belongs to
+            stateString = dataManagerImpl.getState(dataMap, "Margao");
+            
+            System.out.println(stateString);
 	}
 }
--- Assignment2/Participant_Workspace/Q1/CityStateLocator/src/com/util/DataManagerImpl.java
+++ Assignment2/Participant_Workspace/Q1/CityStateLocator/src/com/util/DataManagerImpl.java
@@ -20,7 +20,7 @@ public class DataManagerImpl implements DataManager {
     @Override
     public Map<String, List<String>> populateCityDataMap(String fileName) throws FileNotFoundException {
         
-        Map<String, List<String>> dataMap = new HashMap<String, List<String>>();
+        Map<String, List<String>> dataMap = new HashMap<>();    // have to define string and list only one side. 
         List <String> valuesList;
         String line = "";
         String[] cityStateStrings;
@@ -36,11 +36,13 @@ public class DataManagerImpl implements DataManager {
                     valuesList = dataMap.get(cityStateStrings[0]); // get list from map using current key(state)
                     
                     if (valuesList == null) {   // if no map entry for current key(state), list will be null
-                        valuesList = new ArrayList<String>();   // allocate mem for new list
+                        valuesList = new ArrayList<>();   // allocate mem for new list
                     }
                     
-                    valuesList.add(cityStateStrings[1]); // add city to the list
-
+                    if (! valuesList.contains(cityStateStrings[1])) {   // check that city not already entered for this state
+                        valuesList.add(cityStateStrings[1]); // add city to the list
+                    }
+                    
                     dataMap.put(cityStateStrings[0], valuesList); // put new/updated list in map
                 }
             }
@@ -58,12 +60,33 @@ public class DataManagerImpl implements DataManager {
 
     @Override
     public List<String> getCities(Map<String, List<String>> stateCityMap, String state) throws InvalidStateException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        List <String> citiesList; // don't have to allocate mem/define because it will be initialized in next step
+        
+        citiesList = stateCityMap.get(state);   // get the citylist based on the key(state)
+        
+        if (citiesList == null) {   // if state doesn't exist, list will be null
+            throw new InvalidStateException();
+        }
+        
+        return citiesList;
     }
 
     @Override
     public String getState(Map<String, List<String>> stateCityMap, String city) throws CityNotFoundException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        List <String> citiesList;
+        
+        
+        for (String state : stateCityMap.keySet()) {
+            citiesList = stateCityMap.get(state);
+            if (citiesList.contains(city)) {
+                return state;
+            }
+        }
+        throw new CityNotFoundException();
     }
 
 }
