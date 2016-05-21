af2554eefbe84dc9d52ac4c948b33c54aa8af1ec
aarongoa <taipan93@gmail.com>
2016-05-22 00:54:53 +0530

    Assignment 2 Q4 done

--- Assignment2/Participant_Workspace/Q4/PhoneBook/src/com/psl/Client.java
+++ Assignment2/Participant_Workspace/Q4/PhoneBook/src/com/psl/Client.java
@@ -1,15 +1,38 @@
 package com.psl;
 
+import com.psl.exception.NoDataFoundException;
 import com.psl.util.PhoneBookContacts;
 import com.psl.util.PhoneBookContactsImpl;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Client {
-	
-	public static void main(String[] args){
-		
-		//test your code by calling methods of the PhoneBookContacts from here
-		
-		PhoneBookContacts contacts=new PhoneBookContactsImpl();
-		
-	}
+
+    public static void main(String[] args) throws NoDataFoundException {
+
+        //test your code by calling methods of the PhoneBookContacts from here
+        PhoneBookContacts contacts = new PhoneBookContactsImpl();
+        List<String> contactDetailsList = new ArrayList<>();
+
+        // Contact names
+        String[] contactNameStrings = {"John Doe", "Behati Prinsloo", "Candice Swanepoel"};
+
+        // Contact details in list 
+        contactDetailsList.add("Goa");
+        contactDetailsList.add("Loutolim");
+        contactDetailsList.add("Margao");
+        contactDetailsList.add("USA");
+
+        // add contacts (populate map)
+        for (String contactName : contactNameStrings) {
+            contacts.addContact(contactName, contactDetailsList);
+        }
+
+        // search for each contact and print returned list
+        for (String contactName : contactNameStrings) {
+            contactDetailsList = contacts.searchContact(contactName);
+            System.out.println(contactName + "--> " + contactDetailsList);
+        }
+
+    }
 }
--- Assignment2/Participant_Workspace/Q4/PhoneBook/src/com/psl/util/PhoneBookContactsImpl.java
+++ Assignment2/Participant_Workspace/Q4/PhoneBook/src/com/psl/util/PhoneBookContactsImpl.java
@@ -5,15 +5,38 @@ import java.util.List;
 import java.util.Map;
 
 import com.psl.exception.NoDataFoundException;
+import java.util.ArrayList;
 
 //Override all the methods of the PhoneBookContacts Interface
 public class PhoneBookContactsImpl implements PhoneBookContacts {
 
-	// use this Map to create the PhoneBook Contacts
-	private Map<String, List<String>> contactMap = new HashMap<String, List<String>>();
-	
-	
-	
-	
-	
+    // use this Map to create the PhoneBook Contacts
+    private Map<String, List<String>> contactMap = new HashMap<String, List<String>>();
+
+    @Override
+    public void addContact(String name, List<String> list) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        contactMap.put(name, list);
+    }
+
+    @Override
+    public Map<String, List<String>> getContactMap() {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return contactMap;
+    }
+
+    @Override
+    public List<String> searchContact(String name) throws NoDataFoundException {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List <String> returnContactList = new ArrayList<>();
+        
+        if (!contactMap.containsKey(name)) {
+            throw new NoDataFoundException();
+        }
+        
+        returnContactList = contactMap.get(name);
+        
+        return returnContactList;
+    }
+
 }
