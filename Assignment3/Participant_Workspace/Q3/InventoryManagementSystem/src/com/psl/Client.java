87c35bbdf93852546377e5a7db8aa79ca925f941
aarongoa <taipan93@gmail.com>
2016-06-11 20:46:41 +0530

    Tried Q3. Didn't complete.

--- Assignment3/Participant_Workspace/Q3/InventoryManagementSystem/src/com/psl/Client.java
+++ Assignment3/Participant_Workspace/Q3/InventoryManagementSystem/src/com/psl/Client.java
@@ -4,15 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.bean.Item;
+import com.exception.NoDataFoundException;
 import com.util.InventoryServiceImpl;
 
 public class Client {
 
 	/**
 	 * @param args
+	 * @throws NoDataFoundException 
 	 */
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoDataFoundException {
 		// Call all the functionalities from here to test your code.
 		List <Item> itemsList = new ArrayList<>();
 		
@@ -20,9 +22,12 @@ public class Client {
 		
 		itemsList = inventoryServiceImpl.readAllItemsFromDb();
 		
-		for (Item item : itemsList) {
-			System.out.println(item.getDescription());
-		}
+		inventoryServiceImpl.calculateExpiryDate(itemsList);
+		
+		inventoryServiceImpl.removeExpiredItems(itemsList);
+		
+		itemsList = inventoryServiceImpl.searchItem("Wheat", itemsList);
+		
 	}
 		
 }
--- Assignment3/Participant_Workspace/Q3/InventoryManagementSystem/src/com/util/InventoryServiceImpl.java
+++ Assignment3/Participant_Workspace/Q3/InventoryManagementSystem/src/com/util/InventoryServiceImpl.java
@@ -1,10 +1,12 @@
 package com.util;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import com.bean.Item;
@@ -17,27 +19,65 @@ public class InventoryServiceImpl implements InventoryService {
 	public List<Item> readAllItemsFromDb() {
 		Item item = new Item();
 		List<Item> itemsList = new ArrayList<>();
-		
+
 		DatabaseConnectionManager databaseConnectionManager = new DatabaseConnectionManager();
 		Connection connection = null;
 		Statement statement = null;
 		ResultSet resultSet = null;
-		
-		
+
 		try {
 			connection = databaseConnectionManager.getConnection();
-			//databaseConnectionManager.closeConnection();
-			
+
 			statement = connection.createStatement();
-			resultSet = statement.executeQuery("Select * from cheese_tbl, milk_tbl, wheat_tbl");
-			
+			resultSet = statement.executeQuery("Select * from cheese_tbl");
+
 			while (resultSet.next()) {
-				item.setDescription(resultSet.getString("Description"));
-				
-				
+				item = new Item();
+
+				item.setId(Integer.parseInt(resultSet.getString("id")));
+				item.setDescription(resultSet.getString("description"));
+				item.setWeight(Float.parseFloat(resultSet.getString("weight")));
+				item.setPrice(Float.parseFloat(resultSet.getString("price")));
+				item.setManufacturingDate(Date.valueOf(resultSet.getString("mfg_date")));
+				item.setUseBeforeMonths(Integer.parseInt(resultSet.getString("UseBeforeInMonths")));
+
 				itemsList.add(item);
 			}
 			
+			// for milk table
+			resultSet = statement.executeQuery("Select * from milk_tbl");
+
+			while (resultSet.next()) {
+				item = new Item();
+
+				item.setId(Integer.parseInt(resultSet.getString("id")));
+				item.setDescription(resultSet.getString("description"));
+				item.setWeight(Float.parseFloat(resultSet.getString("weight")));
+				item.setPrice(Float.parseFloat(resultSet.getString("price")));
+				item.setManufacturingDate(Date.valueOf(resultSet.getString("mfg_date")));
+				item.setUseBeforeMonths(Integer.parseInt(resultSet.getString("UseBeforeInMonths")));
+
+				itemsList.add(item);
+			}
+
+			// for wheat table
+			resultSet = statement.executeQuery("Select * from wheat_tbl");
+
+			while (resultSet.next()) {
+				item = new Item();
+
+				item.setId(Integer.parseInt(resultSet.getString("id")));
+				item.setDescription(resultSet.getString("description"));
+				item.setWeight(Float.parseFloat(resultSet.getString("weight")));
+				item.setPrice(Float.parseFloat(resultSet.getString("price")));
+				item.setManufacturingDate(Date.valueOf(resultSet.getString("mfg_date")));
+				item.setUseBeforeMonths(Integer.parseInt(resultSet.getString("UseBeforeInMonths")));
+
+				itemsList.add(item);
+			}
+
+			databaseConnectionManager.closeConnection();
+
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -45,38 +85,76 @@ public class InventoryServiceImpl implements InventoryService {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return itemsList;
 	}
 
 	@Override
 	public void calculateExpiryDate(List<Item> items) {
-		// TODO Auto-generated method stub
 		
+		Date date = null;
+		Calendar calendar = Calendar.getInstance();
+		
+		for (Item item : items) {
+			
+			date = (Date) item.getManufacturingDate();
+			
+			calendar.setTime(date);
+			
+			calendar.add(Calendar.MONTH, item.getUseBeforeMonths());
+			
+			item.setExpiryDate(calendar.getTime());
+		}
+
 	}
 
 	@Override
 	public void removeExpiredItems(List<Item> items) {
 		// TODO Auto-generated method stub
 		
+		Date date = null;
+		Calendar calendar = Calendar.getInstance();
+		
+		for (Item item : items) {
+			
+			date = (Date) item.getManufacturingDate();
+			
+			if (calendar.before(date)) {
+				items.remove(item);
+			}
+			
+		}
+
 	}
 
 	@Override
 	public void sortItems(List<Item> items) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void applyDiscount(List<Item> items) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public List<Item> searchItem(String ItemType, List<Item> list) throws NoDataFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
 		
+		List <Item> itemsList = new ArrayList<>();
+		
+		for (Item item : list) {
+			if (item.getDescription().contains(ItemType)) {
+				itemsList.add(item);
+			}
+		}
+		
+		if (itemsList.size() < 1) {
+			throw new NoDataFoundException();
+		}
+		
+		return itemsList;
+	}
+
 }
